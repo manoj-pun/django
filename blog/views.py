e66922b3900e636b723e7d1625a30fa93ef74743
manoj-pun <manojpun840@gmail.com>
2026-02-10 07:29:36 +0545

    loaded the data

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        "author":"Manoj Pun",
+        "title":"Blog Post 1",
+        "content":"First Post Content",
+        "date_posted":"August 27,2025"
+    },
+    {
+        "author":"David Pun",
+        "title":"Blog Post 2",
+        "content":"Second Post Content",
+        "date_posted":"August 28,2025"
+    }
+]
+
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
     return render(request, "blog/about.html")
