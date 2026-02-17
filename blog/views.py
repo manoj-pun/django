3d3a9623c41ea3659f2c583e5760586d8b667775
manoj-pun <manojpun840@gmail.com>
2026-02-17 10:06:34 +0545

    create: create postform template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 def home(request):
@@ -17,6 +17,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+class PostCreateView(CreateView):
+    model = Post
+    fields = ["title","content"]
 
 def about(request):
     return render(request, "blog/about.html", {"title":"About"})
