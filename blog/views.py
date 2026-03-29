b5247f5453803bbb04917654298ea62b0689e39b
manoj-pun <manojpun840@gmail.com>
2026-03-29 17:40:14 +0545

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        "author":"Manoj Pun",
+        "title":"Blog Post 1",
+        "content":"First post content",
+        "date_posted":"March 29, 2026"
+    },
+    {
+        "author":"David Pun",
+        "title":"Blog Post 2",
+        "content":"Second post content",
+        "date_posted":"March 29, 2026"
+    },
+]
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html",context,)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html",{"title":"About"})
