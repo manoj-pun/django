554c071bf57fb792a6971a4fca05e260b5f45e7a
manoj-pun <manojpun840@gmail.com>
2026-03-30 14:19:46 +0545

    databases

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        "author":"Manoj Pun",
-        "title":"Blog Post 1",
-        "content":"First post content",
-        "date_posted":"March 29, 2026"
-    },
-    {
-        "author":"David Pun",
-        "title":"Blog Post 2",
-        "content":"Second post content",
-        "date_posted":"March 29, 2026"
-    },
-]
+# posts = [
+#     {
+#         "author":"Manoj Pun",
+#         "title":"Blog Post 1",
+#         "content":"First post content",
+#         "date_posted":"March 29, 2026"
+#     },
+#     {
+#         "author":"David Pun",
+#         "title":"Blog Post 2",
+#         "content":"Second post content",
+#         "date_posted":"March 29, 2026"
+#     },
+# ]
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "blog/home.html",context,)
 
